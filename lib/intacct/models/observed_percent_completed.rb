34b7573c751f64f89cf3a31d88bef7c7e4dfd537
Luke Walsh <lwalsh@mavenlink.com>
2019-11-19 10:07:39 -0800

    Add projectkey as a creatable attribute for observed_percent_completed
    - When creating an observed_percent_completed object for a project we need to use projectkey instead of taskkey

--- lib/intacct/models/observed_percent_completed.rb
+++ lib/intacct/models/observed_percent_completed.rb
@@ -5,11 +5,12 @@ module Intacct
       api_name "OBSPCTCOMPLETED"
 
       def create_xml(xml)
-        xml.type      attributes.type
-        xml.taskkey   attributes.taskkey
-        xml.ASOFDATE  attributes.asofdate
-        xml.percent   attributes.percent
-        xml.note      attributes.note
+        xml.type       attributes.type
+        xml.taskkey    attributes.taskkey
+        xml.projectkey attributes.projectkey
+        xml.ASOFDATE   attributes.asofdate
+        xml.percent    attributes.percent
+        xml.note       attributes.note
       end
 
       def update_xml(xml)
