435586472a2f308956292e557f904d91f772df3a
Kevin Cotugno <kcotugno@mavenlink.com>
2020-08-10 13:13:37 -0700

    Add generated task model

--- lib/intacct/models/task.rb
+++ lib/intacct/models/task.rb
@@ -1,56 +1,104 @@
 module Intacct
   module Models
     class Task < Intacct::Base
+      CREATE_KEYS = [:taskname, :projectid, :taskid, :pbegindate, :penddate, :itemid, :billable, :taskdescription, :ismilestone, :utilized, :priority, :taskno, :taskstatus, :parentkey, :parenttaskname, :budgetqty, :estqty, :timetype, :obspercentcomplete, :taskresources, :customfields, :classid, :supdocid, :dependentonkey, :dependentonname].freeze
+      UPDATE_KEYS = [:taskname, :projectid, :pbegindate, :penddate, :itemid, :billable, :taskdescription, :ismilestone, :utilized, :priority, :taskno, :taskstatus, :parentkey, :parenttaskname, :budgetqty, :estqty, :timetype, :obspercentcomplete, :taskresources, :customfields, :classid, :dependentonkey, :dependentonname].freeze
 
       def create_xml(xml)
-        xml.recordno           attributes.recordno if attributes.recordno
-        xml.name               attributes.name
-        xml.description        attributes.description
-        xml.projectid          attributes.projectid
-
-        xml.PBEGINDATE         attributes.pbegindate.try(:strftime, '%m/%d/%Y')
-        xml.PENDDATE           attributes.penddate.try(:strftime, '%m/%d/%Y')
-
-        xml.itemid             attributes.itemid
-        xml.billable           attributes.billable
-        xml.taxdescription     attributes.taxdescription
-        xml.ismilestone        attributes.ismilestone
-        xml.utilized           attributes.utilized
-        xml.priority           attributes.priority
-        xml.taskno             attributes.taskno
-        xml.taskstatus         attributes.taskstatus
-        xml.parenttaskname     attributes.parenttaskname
-        xml.budgetqty          attributes.budgetqty
-        xml.estqty             attributes.estqty
-
-        # Custom field for Mavenlink
-        # TODO(AB): Remove this later
-        xml.mavenlink_id attributes.mavenlink_id
-
-        if attributes.taskresources
-          xml.taskresources {
-            attributes.taskresources.each do |taskresource|
-              xml.employeeid taskresource[:employeeid]
-            end
-          }
-        end
-
-        if attributes.customfields
-          xml.customfields {
-            attributes.customfields.each do |customfield|
-              xml.customfield {
+        xml.TASKNAME attributes.taskname
+        xml.PROJECTID attributes.projectid
+        xml.TASKID attributes.taskid if attributes.taskid.present?
+        xml.PBEGINDATE attributes.pbegindate.strftime("%m/%d/%Y") if attributes.pbegindate.present?
+        xml.PENDDATE attributes.penddate.strftime("%m/%d/%Y") if attributes.penddate.present?
+        xml.ITEMID attributes.itemid if attributes.itemid.present?
+        xml.BILLABLE attributes.billable if attributes.billable.present?
+        xml.TASKDESCRIPTION attributes.taskdescription if attributes.taskdescription.present?
+        xml.ISMILESTONE attributes.ismilestone if attributes.ismilestone.present?
+        xml.UTILIZED attributes.utilized if attributes.utilized.present?
+        xml.PRIORITY attributes.priority if attributes.priority.present?
+        xml.TASKNO attributes.taskno if attributes.taskno.present?
+        xml.TASKSTATUS attributes.taskstatus if attributes.taskstatus.present?
+        xml.PARENTKEY attributes.parentkey if attributes.parentkey.present?
+        xml.PARENTTASKNAME attributes.parenttaskname if attributes.parenttaskname.present?
+        xml.BUDGETQTY attributes.budgetqty if attributes.budgetqty.present?
+        xml.ESTQTY attributes.estqty if attributes.estqty.present?
+        xml.TIMETYPE attributes.timetype if attributes.timetype.present?
+        xml.OBSPERCENTCOMPLETE attributes.obspercentcomplete if attributes.obspercentcomplete.present?
+        xml.TASKRESOURCES attributes.taskresources if attributes.taskresources.present?
+              if attributes.customfields.present? || attributes.to_h.except(*CREATE_KEYS).present?
+        xml.customfields do
+          if attributes.customfields.present?
+            attributes.customfields.presence&.each do |custom_field|
+              xml.customfield do
                 xml.customfieldname customfield[:customfieldname]
                 xml.customfieldvalue customfield[:customfieldvalue]
-              }
+              end
             end
-          }
+          end
+
+          if attributes.to_h.except(*CREATE_KEYS).present?
+            attributes.to_h.except(*CREATE_KEYS).each do |name, value|
+              xml.customfield do
+                xml.customfieldname name
+                xml.customfieldvalue value
+              end
+            end
+          end
         end
+      end
+
+        xml.CLASSID attributes.classid if attributes.classid.present?
+        xml.SUPDOCID attributes.supdocid if attributes.supdocid.present?
+        xml.DEPENDENTONKEY attributes.dependentonkey if attributes.dependentonkey.present?
+        xml.DEPENDENTONNAME attributes.dependentonname if attributes.dependentonname.present?
       end
 
       def update_xml(xml)
-        create_xml(xml)
+        xml.TASKNAME attributes.taskname if attributes.taskname.present?
+        xml.PROJECTID attributes.projectid if attributes.projectid.present?
+        xml.PBEGINDATE attributes.pbegindate.strftime("%m/%d/%Y") if attributes.pbegindate.present?
+        xml.PENDDATE attributes.penddate.strftime("%m/%d/%Y") if attributes.penddate.present?
+        xml.ITEMID attributes.itemid if attributes.itemid.present?
+        xml.BILLABLE attributes.billable if attributes.billable.present?
+        xml.TASKDESCRIPTION attributes.taskdescription if attributes.taskdescription.present?
+        xml.ISMILESTONE attributes.ismilestone if attributes.ismilestone.present?
+        xml.UTILIZED attributes.utilized if attributes.utilized.present?
+        xml.PRIORITY attributes.priority if attributes.priority.present?
+        xml.TASKNO attributes.taskno if attributes.taskno.present?
+        xml.TASKSTATUS attributes.taskstatus if attributes.taskstatus.present?
+        xml.PARENTKEY attributes.parentkey if attributes.parentkey.present?
+        xml.PARENTTASKNAME attributes.parenttaskname if attributes.parenttaskname.present?
+        xml.BUDGETQTY attributes.budgetqty if attributes.budgetqty.present?
+        xml.ESTQTY attributes.estqty if attributes.estqty.present?
+        xml.TIMETYPE attributes.timetype if attributes.timetype.present?
+        xml.OBSPERCENTCOMPLETE attributes.obspercentcomplete if attributes.obspercentcomplete.present?
+        xml.TASKRESOURCES attributes.taskresources if attributes.taskresources.present?
+              if attributes.customfields.present? || attributes.to_h.except(*UPDATE_KEYS).present?
+        xml.customfields do
+          if attributes.customfields.present?
+            attributes.customfields.presence&.each do |custom_field|
+              xml.customfield do
+                xml.customfieldname customfield[:customfieldname]
+                xml.customfieldvalue customfield[:customfieldvalue]
+              end
+            end
+          end
+
+          if attributes.to_h.except(*UPDATE_KEYS).present?
+            attributes.to_h.except(*UPDATE_KEYS).each do |name, value|
+              xml.customfield do
+                xml.customfieldname name
+                xml.customfieldvalue value
+              end
+            end
+          end
+        end
       end
 
+        xml.CLASSID attributes.classid if attributes.classid.present?
+        xml.DEPENDENTONKEY attributes.dependentonkey if attributes.dependentonkey.present?
+        xml.DEPENDENTONNAME attributes.dependentonname if attributes.dependentonname.present?
+      end
     end
   end
 end
