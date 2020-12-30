92b8f5d5301ba1b482b5f34e6f64848388ca608b
John Flores <jflores@mavenlink.com>
2020-12-30 15:43:27 -0800

    Support legacy updates
    This also introduces the ability to send in only the attributes you want to update instead of sending all object attributes in the update call.
    
    I'm not sure if this initial pattern is the best because it's not generic and needs to be supported in each models `update_xml` method. I will hopefully revisit this to see if there is a more generic way to only send the attributes that you want to update.
    
    This commit also properly extracts legacy response errors and status.

--- lib/intacct/actions/update.rb
+++ lib/intacct/actions/update.rb
@@ -1,16 +1,30 @@
 module Intacct
   module Actions
     class Update < Base
-      def request(_options)
-        Intacct::XmlRequest.build_xml(client, action) do |xml|
-          xml.function(controlid: "1") do
-            xml.update do
-              xml.send(klass.api_name) do
-                klass.update_xml(xml)
+      def request(options = {})
+        if legacy?
+          Intacct::XmlRequest.build_xml(client, action) do |xml|
+            xml.function(controlid: "1") do
+              xml.send(klass.legacy_update_name, key: klass.public_send(klass.id_attribute)) do
+                update_with_options? ? klass.update_xml(xml, options) : klass.update_xml(xml)
+              end
+            end
+          end
+        else
+          Intacct::XmlRequest.build_xml(client, action) do |xml|
+            xml.function(controlid: "1") do
+              xml.update do
+                xml.send(klass.api_name) do
+                  update_with_options? ? klass.update_xml(xml, options) : klass.update_xml(xml)
+                end
               end
             end
           end
         end
+      end
+
+      def success?
+        super || (status = @response.at("//response//status")) && status.content == "success"
       end
 
       def response_body
@@ -22,10 +36,10 @@ module Intacct
       end
 
       def response_errors
-        raw = @response.at("//result/errormessage")
+        raw = @response.at("//result/errormessage") || @response.at("//errormessage")
         return unless raw
 
-        Hash.from_xml(raw.to_xml)["errormessage"]["error"]
+        [Hash.from_xml(raw.to_xml)["errormessage"]["error"]].flatten
       end
 
       module Helper
@@ -33,9 +47,16 @@ module Intacct
           response = Intacct::Actions::Update.new(client, self, "update", options).perform
 
           @errors = response.errors
+          @raw_response = response.raw_response
 
           response.success?
         end
+      end
+
+      private
+
+      def update_with_options?
+        klass.method(:update_xml).arity != 1
       end
     end
   end
